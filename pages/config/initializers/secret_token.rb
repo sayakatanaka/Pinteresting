d550b12edfe2c3cdbaa9f3b57c202c5dfbcf6dd4
sayakatanaka <sayaka.5mth@gmail.com>
2015-07-28 10:53:15 +0900

    creating home and about pages

---
+++ pages/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pages::Application.config.secret_token = '1bf6d7a95e64c986458be4074d1630e3bde9d70e07fea535b8b9d5540c815d68ed496cf019b3a5c24506a6e5f2f41a5121e14f08d4496bcdacd1fd4bdc20b49e'
